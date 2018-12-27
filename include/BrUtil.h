dae078456a29e7d26ae7d5c4748a7abb99edd925
Thor Correia <thor.lucas@icloud.com>
2018-12-27 02:38:12 -0800

    vector2 types and their operators

--- include/BrUtil.h
+++ include/BrUtil.h
@@ -1,60 +1,212 @@
 #ifndef _BR_UTIL_H_
 #define _BR_UTIL_H_
 
-#include <functional>
+#include <type_traits>
 #include <SDL2/SDL.h>
+
+#define T_IS_ARITHMETIC typename = typename std::enable_if<std::is_arithmetic<T>::value, T>::type
+#define N_IS_ARITHMETIC typename = typename std::enable_if<std::is_arithmetic<N>::value, N>::type
 
 namespace BromineEngine {
 
-/**
- * This is a type of position object whose coordinates are references.
- * Usually the referene is to an SDL_Rect's x and y.
- */
+/////////////////////////////////////////////////////////////////
+// VECTOR 2D
+/////////////////////////////////////////////////////////////////
+
+template <typename T, T_IS_ARITHMETIC>
 struct Vector2 {
 	union {
-		int x;
-		int w;
+		T x;
+		T w;
 	};
 	union {
-		int y;
-		int h;
+		T y;
+		T h;
 	};
-	Vector2(int _x, int _y) : x(_x), y(_y) {} 
+	Vector2(T _x, T _y) : x(_x), y(_y) {} 
 	Vector2() : Vector2(0, 0) {}
 };
 
+// Vector addition
+template <typename T, T_IS_ARITHMETIC>
+Vector2<T> operator+(const Vector2<T>& left, const Vector2<T>& right) {
+	return Vector2<T>(left.x + right.x, left.y + right.y);
+}
+template <typename T, T_IS_ARITHMETIC>
+Vector2<T>& operator+=(Vector2<T>& left, const Vector2<T>& right) {
+	left.x += right.x; left.y += right.y;
+	return left;
+}
+
+// Vector subtraction
+template <typename T, T_IS_ARITHMETIC>
+Vector2<T> operator-(const Vector2<T>& left, const Vector2<T>& right) {
+	return Vector2<T>(left.x - right.x, left.y - right.y);
+}
+template <typename T, T_IS_ARITHMETIC>
+Vector2<T>& operator-=(Vector2<T>& left, const Vector2<T>& right) {
+	left.x -= right.x; left.y -= right.y;
+	return left;
+}
+
+
+// Dot product
+template <typename T, T_IS_ARITHMETIC>
+T operator*(const Vector2<T>& left, const Vector2<T>& right) {
+	return left.x * right.x + left.y * right.y;
+}
+
+// Scalar multiplication
+template <typename T, T_IS_ARITHMETIC, typename N, N_IS_ARITHMETIC>
+Vector2<T> operator*(N left, const Vector2<T>& right) {
+	return Vector2<T>(left * right.x, left * right.y);
+}
+template <typename T, T_IS_ARITHMETIC, typename N, N_IS_ARITHMETIC>
+Vector2<T> operator*(const Vector2<T>& left, N right) { return right * left; }
+template <typename T, T_IS_ARITHMETIC, typename N, N_IS_ARITHMETIC>
+Vector2<T>& operator*=(Vector2<T>& left, N right) {
+	left.x *= right; left.y *= right;
+	return left;
+}
+
+// Scalar division
+template <typename T, T_IS_ARITHMETIC, typename N, N_IS_ARITHMETIC>
+Vector2<T> operator/(const Vector2<T>& left, N right) {
+	Vector2<T>(left.x / right, left.y / right);
+}
+template <typename T, T_IS_ARITHMETIC, typename N, N_IS_ARITHMETIC>
+Vector2<T>& operator/=(Vector2<T>& left, N right) {
+	left.x /= right; left.y /= right;
+	return left;
+}
+
+typedef Vector2<int> Vector2i;
+typedef Vector2<float> Vector2f;
+
+
+/////////////////////////////////////////////////////////////////
+// VECTOR 2D REFERENCE TYPE
+/////////////////////////////////////////////////////////////////
+
+
+template <typename T, T_IS_ARITHMETIC>
 struct Vector2Ref {
 	union {
-		// Have to use this struct to get around rule against reference in union
-		struct { int& x; };
-		struct { int& w; };
+		struct { T& x; };
+		struct { T& w; };
 	};
 	union {
-		struct { int& y; };
-		struct { int& h; };
+		struct { T& y; };
+		struct { T& h; };
 	};
-	Vector2Ref(int& _x, int& _y) : x(_x), y(_y) {}
+	Vector2Ref(T& _x, T& _y) : x(_x), y(_y) {} 
 
-	Vector2Ref& operator=(Vector2Ref pr) {
-		x = pr.x; y = pr.y;
+	// Assignment
+	Vector2Ref<T>& operator=(const Vector2Ref<T>& right) {
+		x = right.x; y = right.y;
 		return *this;
 	}
-
-	Vector2Ref& operator=(Vector2 p) {
-		x = p.x; y = p.y;
+	Vector2Ref<T>& operator=(const Vector2<T>& right) {
+		x = right.x; y = right.y;
 		return *this;
 	}
 };
 
-struct Rect : public SDL_Rect {
-	Vector2Ref size;
-	Vector2Ref position;
-	Rect(int _x, int _y, int _w, int _h) : size(w, h), position(x, y) {
+// Vector addition
+template <typename T, T_IS_ARITHMETIC>
+Vector2<T> operator+(const Vector2Ref<T>& left, const Vector2<T>& right) {
+	return Vector2<T>(left.x + right.x, left.y + right.y);
+}
+template <typename T, T_IS_ARITHMETIC>
+Vector2<T> operator+(const Vector2<T>& left, const Vector2Ref<T>& right) {
+	return Vector2<T>(left.x + right.x, left.y + right.y);
+}
+template <typename T, T_IS_ARITHMETIC>
+Vector2Ref<T>& operator+=(Vector2Ref<T>& left, const Vector2<T>& right) {
+	left.x += right.x; left.y += right.y;
+	return left;
+}
+template <typename T, T_IS_ARITHMETIC>
+Vector2Ref<T>& operator+=(Vector2Ref<T>& left, const Vector2Ref<T>& right) {
+	left.x += right.x; left.y += right.y;
+	return left;
+}
+
+// Vector subtraction
+template <typename T, T_IS_ARITHMETIC>
+Vector2<T> operator-(const Vector2Ref<T>& left, const Vector2<T>& right) {
+	return Vector2<T>(left.x - right.x, left.y - right.y);
+}
+template <typename T, T_IS_ARITHMETIC>
+Vector2<T> operator-(const Vector2<T>& left, const Vector2Ref<T>& right) {
+	return Vector2<T>(left.x - right.x, left.y - right.y);
+}
+template <typename T, T_IS_ARITHMETIC>
+Vector2Ref<T>& operator-=(Vector2Ref<T>& left, const Vector2<T>& right) {
+	left.x -= right.x; left.y -= right.y;
+	return left;
+}
+template <typename T, T_IS_ARITHMETIC>
+Vector2Ref<T>& operator-=(Vector2Ref<T>& left, const Vector2Ref<T>& right) {
+	left.x -= right.x; left.y -= right.y;
+	return left;
+}
+
+
+// Dot product
+template <typename T, T_IS_ARITHMETIC>
+T operator*(const Vector2Ref<T>& left, const Vector2<T>& right) {
+	return left.x * right.x + left.y * right.y;
+}
+template <typename T, T_IS_ARITHMETIC>
+T operator*(const Vector2<T>& left, const Vector2Ref<T>& right) {
+	return left.x * right.x + left.y * right.y;
+}
+template <typename T, T_IS_ARITHMETIC>
+T operator*(const Vector2Ref<T>& left, const Vector2Ref<T>& right) {
+	return left.x * right.x + left.y * right.y;
+}
+
+// Scalar multiplication
+template <typename T, T_IS_ARITHMETIC, typename N, N_IS_ARITHMETIC>
+Vector2<T> operator*(N left, const Vector2Ref<T>& right) {
+	return Vector2<T>(left * right.x, left * right.y);
+}
+template <typename T, T_IS_ARITHMETIC, typename N, N_IS_ARITHMETIC>
+Vector2<T> operator*(const Vector2Ref<T>& left, N right) { return right * left; }
+template <typename T, T_IS_ARITHMETIC, typename N, N_IS_ARITHMETIC>
+Vector2Ref<T>& operator*=(Vector2Ref<T>& left, N right) {
+	left.x *= right; left.y *= right;
+	return left;
+}
+
+// Scalar division
+template <typename T, T_IS_ARITHMETIC, typename N, N_IS_ARITHMETIC>
+Vector2<T> operator/(const Vector2Ref<T>& left, N right) {
+	Vector2<T>(left.x / right, left.y / right);
+}
+template <typename T, T_IS_ARITHMETIC, typename N, N_IS_ARITHMETIC>
+Vector2Ref<T>& operator/=(Vector2Ref<T>& left, N right) {
+	left.x /= right; left.y /= right;
+	return left;
+}
+
+typedef Vector2Ref<int> Vector2Refi;
+typedef Vector2Ref<float> Vector2Reff;
+
+/////////////////////////////////////////////////////////////////
+// RECTANGLES
+/////////////////////////////////////////////////////////////////
+
+struct Recti : public SDL_Rect {
+	Vector2Refi size;
+	Vector2Refi position;
+	Recti(int _x, int _y, int _w, int _h) : size(w, h), position(x, y) {
 		x = _x; y = _y;
 		w = _w; h = _h;
 	}
-	Rect() : Rect(0, 0, 0, 0) {}
-	Rect(Vector2 _position, Vector2 _size) : Rect(_position.x, _position.y, _size.w, _size.h) {}
+	Recti() : Recti(0, 0, 0, 0) {}
+	Recti(Vector2i _position, Vector2i _size) : Recti(_position.x, _position.y, _size.w, _size.h) {}
 };
 	
 }
